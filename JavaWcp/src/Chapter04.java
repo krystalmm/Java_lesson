85b9a4ed7867eb173d6e4d9761c8cd2137d913f7
krystalmm <taeyeon_aina@live.jp>
2021-06-10 07:01:52 +0900

    [Finish] lesson4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+
+	public static void main(String[] args) {
+		final int NUMBER = 7;
+		
+		String name = "mana";
+		System.out.println(name);
+		
+		name = "YOLO!";
+		System.out.println(name);
+		
+		System.out.println(NUMBER);
+	}
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,21 @@
+
+public class Variable01 {
+	
+	public static void main(String[] args) {
+//		型を指定して変数を宣言する
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+		
+//		値の再代入
+		num1 = 20;
+		System.out.println(num1);
+		
+//		変数の宣言と初期化を同時にすることもできる！
+		int num2 = 30;
+		System.out.println(num2);
+	}
+}
