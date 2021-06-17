c9167a04c8b463da8acf1b1308b10878ea5921b3
krystalmm <taeyeon_aina@live.jp>
2021-06-17 22:36:01 +0900

    [Finish] lesson24

--- DB/src/JdbcSelectSample.java
+++ DB/src/JdbcSelectSample.java
@@ -67,7 +67,7 @@ public class JdbcSelectSample {
 //	実際に上記処理をまとめると以下のようになる！
 	
 //	DriverとURLは定数にしておく！
-	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/database01?user=user01&password=password01&useSSL=false";
 	
 	public static void main(String[] args) throws ClassNotFoundException {
@@ -102,4 +102,32 @@ public class JdbcSelectSample {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	
+//	パラメータ付きのSQLの発行
+//	where句を使って条件を絞って取得する方法を実装する！
+//	例えば、select * from user where id = 1　のようにidが1のレコードだけを取得してみる！
+	
+//	上記との変更点はまずPreparedStatement！
+//	PreparedStatement statement = connection.prepareStatement("select * from user where id = ?");
+//	発行したいSQLはwhere id = 1だが、?を使って表記する！この？をプレースホルダという！!
+//	この場合はパラメータを入れてあげる必要がある！
+//	statement.executeQuery()の部分！
+//	statement.setLong(1, 1L);
+//	このテーブルのカラムidはデータベースのint型なので、JavaではLong型になるので、setLongでLong型のパラメータを渡す！(varcharだったらsetString)
+//	setLongの第一引数は、?が登場する順番を指定する！
+//	第二引数は、SQLに埋め込むパラメータそのものを指定！（この場合は、1でLong型なので、1Lを指定！）
+	
+//	String型のパラメータを渡したいときの例(select * from user where name = "山田太郎")
+//	select * from user where name = ?
+//	statement.setString(1, "山田太郎");
+	
+	
+	
+//	データ登録・更新・削除
+	
+//	SQLのinsert文を使ってデータの登録を行う！(Connectionを作るところまではデータの抽出と同じ！)
+//	Class.forName("com.mysql.cj.jdbc.Driver");
+	
 }
