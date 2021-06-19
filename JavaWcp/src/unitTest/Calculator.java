c7744973689c814a18b9ffd04457d2bd28ce6637
krystalmm <taeyeon_aina@live.jp>
2021-06-19 23:31:46 +0900

    [Start] JUnit lesson

---
+++ JavaWcp/src/unitTest/Calculator.java
@@ -0,0 +1,9 @@
+package unitTest;
+
+public class Calculator {
+	
+//	引数に与えられた数値を二乗して返すメソッド
+	public int square(int x) {
+		return x * x;
+	}
+}
---
+++ JavaWcp/src/unitTest/CalculatorTest.java
@@ -0,0 +1,14 @@
+package unitTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
